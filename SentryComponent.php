a1fba1f64439550e335f2e4c07ae4045206e29cb
erback <erik.n.back@gmail.com>
2020-04-29 07:12:55 +0200

    removed support for raven js due to problems with bower installs

--- SentryComponent.php
+++ SentryComponent.php
@@ -28,27 +28,6 @@ class SentryComponent extends CApplicationComponent
 	public $options = array();
 
 	/**
-	 * Publish, register and configure Raven-JS.
-	 * @see https://raven-js.readthedocs.org/
-	 * @var bool
-	 */
-	public $useRavenJs = false;
-
-	/**
-	 * Raven-JS configuration options.
-	 * @var array
-	 * @see https://raven-js.readthedocs.org/en/latest/config/index.html#optional-settings
-	 */
-	public $ravenJsOptions = array();
-
-	/**
-	 * Raven-JS plugins.
-	 * @var array
-	 * @see https://raven-js.readthedocs.org/en/latest/plugins/index.html
-	 */
-	public $ravenJsPlugins = array();
-
-	/**
 	 * Initialize Raven_ErrorHandler.
 	 * @var bool
 	 */
@@ -65,9 +44,7 @@ class SentryComponent extends CApplicationComponent
 	public function init()
 	{
 		parent::init();
-		if ($this->useRavenJs) {
-			$this->registerRavenJs();
-		}
+
 		if ($this->useRavenErrorHandler) {
 			$this->registerRavenErrorHandler();
 		}
@@ -86,50 +63,6 @@ class SentryComponent extends CApplicationComponent
 		return $this->raven;
 	}
 
-	/**
-	 * Register and configure Raven js.
-	 * @param array|null $options If null, then will be used $this->ravenJsOptions.
-	 * @param array|null $plugins If null, then will be used $this->ravenJsPlugins.
-	 * @param array|null $context If null, then will be used $this->getUserContext().
-	 * @return bool
-	 * @throws CException
-	 */
-	public function registerRavenJs(array $options = null, array $plugins = null, array $context = null)
-	{
-		/** @var CAssetManager $assetManager */
-		$assetManager = $this->getComponent('assetManager');
-		/** @var CClientScript $clientScript */
-		$clientScript = $this->getComponent('clientScript');
-
-		if (!$assetManager || !$clientScript) {
-			return false;
-		}
-
-		$jsOptions = $options !== null ? $options : $this->ravenJsOptions;
-		$jsPlugins = $plugins !== null ? $plugins : $this->ravenJsPlugins;
-		$jsContext = $context !== null ? $context : $this->getUserContext();
-
-		$assetUrl = $assetManager->publish(__DIR__ . '/../../bower-asset/raven-js/dist');
-		$clientScript
-			->registerScriptFile($assetUrl . '/raven.js', CClientScript::POS_HEAD)
-			->registerScript(
-				'raven-js1',
-				'Raven.config(\'' . $this->getJsDsn() . '\', ' . CJavaScript::encode($jsOptions) . ').install();',
-				CClientScript::POS_BEGIN
-			);
-
-		if ($jsContext) {
-			$clientScript->registerScript(
-				'raven-js2',
-				'Raven.setUserContext(' . CJavaScript::encode($jsContext) . ');',
-				CClientScript::POS_BEGIN
-			);
-		}
-
-		foreach ($jsPlugins as $plugin) {
-			$clientScript->registerScriptFile($assetUrl . '/plugins/' . $plugin . '.js', CClientScript::POS_HEAD);
-		}
-	}
 
 	/**
 	 * Initialize raven client.
@@ -141,15 +74,6 @@ class SentryComponent extends CApplicationComponent
 		if ($userContext = $this->getUserContext()) {
 			$this->raven->user_context($userContext);
 		}
-	}
-
-	/**
-	 * Return Dsn without security token.
-	 * @return string
-	 */
-	protected function getJsDsn()
-	{
-		return preg_replace('#:\w+@#', '@', $this->dsn);
 	}
 
 	/**
